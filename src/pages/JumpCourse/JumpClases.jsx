abf809a04fbf734bf0b973b2edc8cc142cc2b902
Fernando Bassino <ferbassino@gmail.com>
2024-03-24 11:52:33 -0300

    video

--- src/pages/JumpCourse/JumpClases.jsx
+++ src/pages/JumpCourse/JumpClases.jsx
@@ -1,16 +1,22 @@
 import React from "react";
+import ReactPlayer from "react-player";
 
+const url =
+  "https://storage.cloud.google.com/kinapp-web/jump-course/2_posicion.mp4";
 const JumpClases = () => {
   return (
     <section className="text-gray-600 body-font">
       <div className="container px-5 py-24 mx-auto">
-        <div className="flex flex-col text-center w-full mb-20">
+        <div className="flex flex-col text-center  mb-20 w-50">
           <h2 className="sm:text-3xl text-xs text-blue-900 tracking-widest font-medium title-font mb-1">
             Tus clases
           </h2>
-          {/* <h1 className="sm:text-3xl text-2xl font-medium title-font text-blue-900">
-            Análisis biomecanico del salto vertical
-          </h1> */}
+          <ReactPlayer
+            url="https://storage.cloud.google.com/kinapp-web/jump-course/2_posicion.mp4"
+            controls
+            width="100%"
+            height="100%"
+          />
         </div>
       </div>
     </section>
