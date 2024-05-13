7cadb874d72ae6c42bb99e6570a51f1320604a3c
Fernando Bassino <ferbassino@gmail.com>
2024-05-13 19:08:09 -0300

    cabiando ruta tambien

--- src/utils/ReaderRoutes.jsx
+++ src/utils/ReaderRoutes.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Outlet } from "react-router-dom";
 
 const ReaderRoutes = ({ roles, redirectPath = "/" }) => {
-  if (roles !== "jumpCourse2024") {
+  if (roles !== "editor") {
     return <Navigate to={redirectPath} replace />;
   }
   return <Outlet />;
