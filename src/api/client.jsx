d8bb9276e5bb27d26368e444f698d3af6991647e
Fernando Bassino <ferbassino@gmail.com>
2024-05-21 23:47:39 -0300

    intento 0

--- src/api/client.jsx
+++ src/api/client.jsx
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-export default axios.create({ baseURL: "https://kinapp-api.vercel.app/" });
-// export default axios.create({ baseURL: "http://localhost:3001/" });
+// export default axios.create({ baseURL: "https://kinapp-api.vercel.app/" });
+export default axios.create({ baseURL: "http://localhost:3001/" });
--- src/views/landing/Products/Products.jsx
+++ src/views/landing/Products/Products.jsx
@@ -7,10 +7,10 @@ import { useState } from "react";
 import Navbar from "../../../components/landing/header/Navbar";
 import Footer from "../../../components/landing/footer/Footer";
 
+console.log("publik key", import.meta.env.VITE_PUBLIC_KEY);
 const Products = () => {
   const [preferenceId, setPreferenceId] = useState(null);
-
-  initMercadoPago(`${import.meta.env.VITE_PUBLIC_KEY}`, {
+  initMercadoPago(`APP_USR-8e90396a-fd28-4c24-81e4-812af239461f`, {
     locale: "es-AR",
   });
 
