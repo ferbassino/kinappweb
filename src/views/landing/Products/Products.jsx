a61beebc54e9d64c707cd125488b26bd697fdc97
Fernando Bassino <ferbassino@gmail.com>
2024-05-21 23:49:29 -0300

    otra 1

--- src/views/landing/Products/Products.jsx
+++ src/views/landing/Products/Products.jsx
@@ -7,7 +7,6 @@ import { useState } from "react";
 import Navbar from "../../../components/landing/header/Navbar";
 import Footer from "../../../components/landing/footer/Footer";
 
-console.log("publik key", import.meta.env.VITE_PUBLIC_KEY);
 const Products = () => {
   const [preferenceId, setPreferenceId] = useState(null);
   initMercadoPago(`APP_USR-8e90396a-fd28-4c24-81e4-812af239461f`, {
